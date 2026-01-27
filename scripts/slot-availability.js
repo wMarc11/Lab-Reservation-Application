91fefe37e69aee8d0f6a139843f99a05fb8bbf10
JustAGuy <jehannabar68@gmail.com>
2026-01-27 13:34:35 +0800

    Added base files

---
+++ scripts/slot-availability.js
@@ -0,0 +1,10 @@
+
+// a form asking which labs/rooms they want to reserve
+// it will go 
+// > which building
+// > which floor
+// > which room/lab
+
+// looking at the slot availability
+// > slot object has availability and a unique slot no that identifies where it is
+// > reservation has a slot id attached to it and contains other details like who reserved it etc
