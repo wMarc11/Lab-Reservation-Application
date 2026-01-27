747d5fc918a7a3f7d2fb1da6f0e04e2ea81dd530
Nykko Vanguardia <nykkovanguardia.dev@gmail.com>
2026-01-28 01:14:58 +0800

    chore: refractor to BEM format

--- test.js
+++ test.js
@@ -2,14 +2,19 @@ const logInTransition = document.getElementById("log-in-transition");
 const signUpTransition  = document.getElementById("sign-up-transition");
 const logInCard = document.getElementById("log-in-card");
 const signInContent = document.getElementById("sign-in-content");
+const hiddableH4 = document.getElementById("hiddable")
 
 logInTransition.addEventListener("click", () => {
-    logInCard.classList.add("active");
-    signInContent.classList.add("no-opacity");
+    hiddableH4.classList.add("is-transparent");
+    logInTransition.classList.add("is-transparent");
+    logInCard.classList.add("form__panel--active");
+    signInContent.classList.add("is-transparent");
 })
 
 signUpTransition.addEventListener("click", () => {
-    logInCard.classList.remove("active")
-    signInContent.classList.remove("no-opacity");
+    hiddableH4.classList.remove("is-transparent");
+    logInTransition.classList.remove("is-transparent");
+    logInCard.classList.remove("form__panel--active")
+    signInContent.classList.remove("is-transparent");
 })
 
