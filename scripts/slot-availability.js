875e243a2a93cf63e20432a1e30577bb7e31bdae
JustAGuy <jehannabar68@gmail.com>
2026-02-01 20:22:36 +0800

    feat: added navigation for dates

--- scripts/slot-availability.js
+++ scripts/slot-availability.js
@@ -8,3 +8,17 @@
 // looking at the slot availability
 // > slot object has availability and a unique slot no that identifies where it is
 // > reservation has a slot id attached to it and contains other details like who reserved it etc
+
+const info_container = document.querySelector('.information-section');
+const heading = info_container.querySelector('h1');
+
+const today = new Date();
+const options = {
+    weekday: 'short',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric'
+};
+
+const formattedDate = today.toLocaleDateString('en-US', options);
+heading.innerText = formattedDate;
