468f21ec936b008348416d8df62261515cbfd760
JustAGuy <jehannabar68@gmail.com>
2026-02-07 22:04:37 +0800

    style: set container as fixed, but table as flexible

--- scripts/slot-availability.js
+++ scripts/slot-availability.js
@@ -1,14 +1,4 @@
 
-// a form asking which labs/rooms they want to reserve
-// it will go 
-// > which building
-// > which floor
-// > which room/lab
-
-// looking at the slot availability
-// > slot object has availability and a unique slot no that identifies where it is
-// > reservation has a slot id attached to it and contains other details like who reserved it etc
-
 const info_container = document.querySelector('.availability-section');
 const heading = info_container.querySelector('h1');
 
