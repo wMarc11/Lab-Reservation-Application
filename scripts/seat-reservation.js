c96aa3b4032703ffc402aa7767a0f2b01ed071e6
JustAGuy <jehannabar68@gmail.com>
2026-02-09 20:29:37 +0800

    feat: added m104d

--- scripts/seat-reservation.js
+++ scripts/seat-reservation.js
@@ -42,9 +42,57 @@ const M101 = `
     </div>
 `
 
-const M101_BACKGROUND = `background-image: url('https://www.dlsu.edu.ph/wp-content/uploads/images/coe/chem_eng/instrumentation-room.JPG');`
-
-const M101A = ``
+const M104D = `
+    <div class="display-seat-row"> 
+    </div>
+    <div class="display-seat-row"> 
+        <svg style="width: 3rem; height: 5rem; margin-left:35.3rem" viewBox="0 0 100 100" preserveAspectRatio="none">
+            <rect width="100" height="700" style="fill:var(--color-info-dark)" />
+        </svg>
+        <svg style="width: 4.5rem; height: 4.5rem" viewBox="0 0 100 100" preserveAspectRatio="none">
+            <circle r="45" cx="50" cy="50" fill="#8FC991" />
+        </svg>
+        <svg style="width: 3rem; height: 5rem" viewBox="0 0 100 100" preserveAspectRatio="none">
+            <rect width="100" height="700" style="fill:var(--color-info-dark)" />
+        </svg>
+        <svg style="width: 4.5rem; height: 4.5rem" viewBox="0 0 100 100" preserveAspectRatio="none">
+            <circle r="45" cx="50" cy="50" fill="#8FC991" />
+        </svg>
+    </div>
+    <div class="display-seat-row"> 
+        <svg style="width: 3rem; height: 5rem; margin-left:35.3rem" viewBox="0 0 100 100" preserveAspectRatio="none">
+            <rect width="100" height="700" style="fill:var(--color-info-dark)" />
+        </svg>
+        <svg style="width: 4.5rem; height: 4.5rem" viewBox="0 0 100 100" preserveAspectRatio="none">
+            <circle r="45" cx="50" cy="50" fill="#8FC991" />
+        </svg>
+        <svg style="width: 3rem; height: 5rem" viewBox="0 0 100 100" preserveAspectRatio="none">
+            <rect width="100" height="700" style="fill:var(--color-info-dark)" />
+        </svg>
+        <svg style="width: 4.5rem; height: 4.5rem" viewBox="0 0 100 100" preserveAspectRatio="none">
+            <circle r="45" cx="50" cy="50" fill="#8FC991" />
+        </svg>
+    </div>
+    <div class="display-seat-row"> 
+        <svg style="width: 3rem; height: 8rem; margin-left:3rem" viewBox="0 0 100 100" preserveAspectRatio="none">
+            <rect width="100" height="700" style="fill:var(--color-info-dark)" />
+        </svg>
+        <svg style="width: 4.5rem; height: 4.5rem" viewBox="0 0 100 100" preserveAspectRatio="none">
+            <circle r="45" cx="50" cy="50" fill="#8FC991" />
+        </svg>
+        </svg>
+    </div>
+    <div class="display-seat-row"> 
+        <svg style="width: 4.5rem; height: 4.5rem; margin-left:42rem" viewBox="0 0 100 100" preserveAspectRatio="none">
+            <circle r="45" cx="50" cy="50" fill="#8FC991" />
+        </svg>
+    </div>
+    <div class="display-seat-row">
+        <svg style="width: 10rem; height: 3rem; margin-left:39.3rem" viewBox="0 0 100 100" preserveAspectRatio="none">
+            <rect width="700" height="100" style="fill:var(--color-info-dark)" />
+        </svg>
+    </div>
+`
 
 
 
