00b7f6bc13bd1bcb3c205c5e2647adcc8fb7d8c4
Marc <marcadrian.bobadilla@gmail.com>
2026-02-12 02:25:53 +0800

    fixed bugs

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -6,3 +6,25 @@ const mm = String(today.getMonth() + 1).padStart(2, '0');
 const dd = String(today.getDate()).padStart(2, '0');
 
 dateInput.value = `${yyyy}-${mm}-${dd}`;
+
+const loggedInUserJSON = sessionStorage.getItem("loggedInUser");
+let loggedInUser = null;
+
+if (loggedInUserJSON) {
+    loggedInUser = JSON.parse(loggedInUserJSON);
+}
+
+if (loggedInUser) {
+    const infoEl = document.querySelector(".profile .info p b");
+    const userTypeEl = document.getElementById("user-type");
+
+    if (infoEl) infoEl.textContent = loggedInUser.firstName; 
+    if (userTypeEl) userTypeEl.textContent = loggedInUser.accountType; 
+}
+
+if (loggedInUser && loggedInUser.accountType === "Admin") {
+    const dashboardLink = document.querySelector('.sidebar a[href="dashboard.html"]');
+    if (dashboardLink) {
+        dashboardLink.href = "dashboard-admin.html";
+    }
+}
