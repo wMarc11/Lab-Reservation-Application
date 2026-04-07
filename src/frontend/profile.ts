b05264e10c3bc786a8146f77d955bd354d467bf8
Nykko Vanguardia <nykkovanguardia.dev@gmail.com>
2026-04-07 23:17:39 +0800

    add user type to profile

--- src/frontend/profile.ts
+++ src/frontend/profile.ts
@@ -185,6 +185,8 @@ async function loadProfile() {
         if (courseEl) courseEl.textContent = user.course;
         const contactNumberEl = document.querySelector('#contactNumber');
         if (contactNumberEl) contactNumberEl.textContent = user.contactNumber;
+        const userType = document.querySelector("#user-typ") as HTMLHeadingElement;
+        if (userType) userType.innerHTML = user.role;
         profileImage.src = `${BASE_URL}/images/${user.profileImage}`;
         inputs.forEach(input => {
             const htmlInput = input as HTMLInputElement;
